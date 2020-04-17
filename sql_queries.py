aa32e51243803b2ceb2bdcab80596b721e8e5b9a
tonysjohn <tonysjohn@github.com>
2020-04-17 13:30:11 -0500

    Initial commit

---
+++ sql_queries.py
@@ -0,0 +1,121 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+
+songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays(songplay_id SERIAL PRIMARY KEY
+                                                                   , start_time timestamp
+                                                                   , user_id int
+                                                                   , level varchar(10)
+                                                                   , song_id varchar(50)
+                                                                   , artist_id varchar(50)
+                                                                   , session_id int
+                                                                   , location varchar
+                                                                   , user_agent varchar);
+""")
+
+user_table_create = (""" CREATE TABLE IF NOT EXISTS users(user_id int PRIMARY KEY
+                                           , first_name varchar(100)
+                                           , last_name varchar(100)
+                                           , gender char
+                                           , level varchar(10));
+""")
+
+song_table_create = (""" CREATE TABLE IF NOT EXISTS songs(song_id varchar(50) PRIMARY KEY
+                                           , title varchar
+                                           , artist_id varchar(50)
+                                           , year int
+                                           , duration int);
+""")
+
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists(artist_id varchar(50) PRIMARY KEY
+                                              , name varchar(100)
+                                              , location varchar(100)
+                                              , latitude float
+                                              , longitude float);
+""")
+
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time(start_time timestamp
+                                           , hour int
+                                           , day int
+                                           , week int
+                                           , month int
+                                           , year int
+                                           , weekday int);
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = (""" INSERT INTO songplays(start_time
+                                                   , user_id
+                                                   , level
+                                                   , song_id
+                                                   , artist_id
+                                                   , session_id
+                                                   , location
+                                                   , user_agent)
+                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+""")
+
+user_table_insert = ("""INSERT INTO users(user_id
+                                           , first_name
+                                           , last_name
+                                           , gender
+                                           , level)
+                        VALUES (%s, %s, %s, %s, %s)
+                        ON CONFLICT(user_id)
+                        DO UPDATE
+                        SET level = EXCLUDED.level
+""")
+
+song_table_insert = ("""INSERT INTO songs(song_id
+                                        , title
+                                        , artist_id
+                                        , year
+                                        , duration)
+                        VALUES (%s, %s, %s, %s, %s)
+                        ON CONFLICT (song_id)
+                        DO NOTHING
+""")
+
+artist_table_insert = ("""INSERT INTO artists(artist_id
+                                              , name
+                                              , location
+                                              , latitude
+                                              , longitude)
+                            VALUES (%s, %s, %s, %s, %s)
+                            ON CONFLICT (artist_id)
+                            DO NOTHING
+""")
+
+
+time_table_insert = ("""INSERT INTO time(start_time
+                                           , hour
+                                           , day
+                                           , week
+                                           , month
+                                           , year
+                                           , weekday)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s)
+                        ON CONFLICT
+                        DO NOTHING
+""")
+
+# FIND SONGS
+
+song_select = ("""SELECT s.song_id, s.artist_id 
+                  FROM songs s LEFT JOIN artists a ON s.artist_id = a.artist_id
+                  WHERE s.title = %s
+                  AND a.name = %s
+                  AND s.duration = %s
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
